fc435490dc3ec2787bd4a2b66c423d8179b587b7
sinan-ozel <4183117+sinan-ozel@users.noreply.github.com>
2021-05-03 16:43:07 -0400

    Add ischaemic attack mortality by state, gender, race and age group

---
+++ get_mortality_from_ischaemic_attack.bat
@@ -0,0 +1,1 @@
+python scripts\get_mortality.py -7 "^41[23]" -8 "^41[0-4]" -9 "^41[0-4]" -10 "^I2[0-5]"  -i ./raw_data -o ./processed_data/ischaemic_attack_mortality_by_state_v3.5.dta -s 1969 -e 2004
